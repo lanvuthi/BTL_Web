58c89238d69765ccead395508cab4c5adac68023
duythien-206 <90381370+duythien-206@users.noreply.github.com>
2021-10-29 10:34:58 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,89 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<link rel="stylesheet prefetch" href="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css"><script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <title>Quản Lý</title>
+</head>
+<body>
+    <div class="header">
+        <nav class="navbar navbar-light bg-light">
+            <h5>Ten User</h5>
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.jpg" class="rounded-circle" width="60" height="60" alt="">
+            </a>
+        </nav>
+    </div>
+    <main>
+        <div class="alert alert-primary" role="alert">
+            Co cong viec "<a href="#" class="alert-link">abc</a>" can hoan thanh vao ngay 112
+        </div>
+        <div class="alert alert-success" role="alert">
+            Co cong viec "<a href="#" class="alert-link">xyz</a>" can hoan thanh vao ngay 465
+        </div>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>sắp xếp thời gian biểu</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
+        <div class="container">
+            <div class="row">
+              <div class="col-5">
+                <div id="datepicker" class="input-group date" data-date-format="dd-mm-yyyy"> 
+                    <p>Chọn ngày: </p>
+                    <input class="form-control" readonly="" type="text"> <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span> 
+                </div>
+                <script type="text/javascript">
+                    $(function () {  
+                    $("#datepicker").datepicker({         
+                    autoclose: true,         
+                    todayHighlight: true 
+                    }).datepicker('update', new Date());
+                    });
+                </script>
+              </div>
+              <div class="col-6">
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+              </div>
+            </div>
+          </div>
+        
+          <table class="table">
+            <thead class="thead-light">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Tieu de</th>
+                <th scope="col">Mieu ta</th>
+                <th scope="col">Dealine</th>
+                <th scope="col">Trang thai</th>
+                <th scope="col">Hanh dong</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>fgnfnfn</td>
+                <td>fnfnf</td>
+                <td>gfnfnf</td>
+                <td>gfnfnf</td>
+                <td>gfnfnf</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>fnfnf</td>
+                <td>oiiiuiou</td>
+                <td>ukhkhk</td>
+                <td>gfnfnf</td>
+                <td>gfnfnf</td>
+              </tr>
+            </tbody>
+          </table>
+    </main>
+</body>
 </html>
